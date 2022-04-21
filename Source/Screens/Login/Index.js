c89a1f6d30133cc4a48f72d42c43579cd7ce1f00
sheikhabdulbasit-nickelfox <sheikh.abdulbasit@nickelfox.com>
2022-04-21 13:12:16 +0530

    feat(login): add btn to navigate to sign up

--- Source/Screens/Login/Index.js
+++ Source/Screens/Login/Index.js
@@ -1,6 +1,48 @@
-import React from "react";
-import { View } from "react-native";
+import {useNavigation} from '@react-navigation/native';
+import React from 'react';
+import {Pressable, View, Text, StyleSheet} from 'react-native';
+import {useTheme} from 'react-native-paper';
 
-export default Login = () => {
-  return <View></View>;
+export default Login = props => {
+  const {colors} = useTheme();
+  const {reset} = useNavigation();
+
+  const navigateToLogin = () => {
+    reset({
+      index: 0,
+      routes: [{name: 'Signup'}],
+    });
+  };
+
+  return (
+    <View style={styles.buttonContainer}>
+      <Pressable
+        onPress={navigateToLogin}
+        style={[styles.button, {backgroundColor: colors.primary}]}
+        android_ripple={{color: 'white', borderless: true}}>
+        <Text style={[styles.buttonLabel]}>Sign Up</Text>
+      </Pressable>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    borderRadius: 10,
+    margin: 10,
+    elevation: 25,
+  },
+  button: {
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFBBBB',
+    height: 40,
+  },
+  buttonLabel: {
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
