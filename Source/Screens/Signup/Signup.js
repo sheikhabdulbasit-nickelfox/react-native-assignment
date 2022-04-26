7e3ec72315de4edd3c2577c20101fd108bbca849
sheikhabdulbasit-nickelfox <sheikh.abdulbasit@nickelfox.com>
2022-04-26 18:39:01 +0530

    refactor(Signup): remove unused imports, add form in new component

--- Source/Screens/Signup/Signup.js
+++ Source/Screens/Signup/Signup.js
@@ -1,26 +1,25 @@
 import React, {Fragment, useState, useRef} from 'react';
 import {View, Pressable, StyleSheet, Dimensions, Image} from 'react-native';
-import {Formik} from 'formik';
+
 import Ionicon from 'react-native-vector-icons/Ionicons';
 import ImagePicker from 'react-native-image-crop-picker';
 import {useTheme} from 'react-native-paper';
 import CommonScrollView from '../../Components/Views/CommonScrollView';
 import Title from '../../Components/Text/PTitle';
-import {
-  SignupInitialValues,
-  SignupValidationModel,
-} from '@models/SignUpValidationModel';
-import FormField from '@components/TextInput/FormField';
+import SignUpFormik from '@components/TextInput/SignUpFormik';
 
 const {height} = Dimensions.get('window');
 
 const SignUp = () => {
-  const {colors, fonts} = useTheme();
+  const {colors} = useTheme();
   const [dpImage, setDpImage] = useState({state: false, uri: null});
-  const [activeFormId, setActiveFormId] = useState('');
+
   const [backdropEnabed, setBackdropEnabled] = useState(false);
 
-  console.log(activeFormId);
+  const submitForm = values => {
+    console.log(values);
+  };
+
   return (
     <Fragment>
       {backdropEnabed && (
@@ -49,81 +48,13 @@ const SignUp = () => {
             style={[styles.profileImage, {backgroundColor: colors.primary}]}
           />
           <Pressable
-            android_ripple={{color: colors.surface, borderless: false}}
-            style={[styles.cameraIcon, {backgroundColor: colors.accent}]}>
+            style={[styles.cameraIcon, {backgroundColor: colors.lightAsh}]}>
             <Ionicon name="md-camera-sharp" size={25} color={colors.surface} />
           </Pressable>
         </View>
 
         {/* Sign Up Form */}
-        <Formik
-          initialValues={SignupInitialValues}
-          validationSchema={SignupValidationModel}
-          validateOnMount
-          onSubmit={values => console.log(values)}>
-          {({
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            values,
-            isValid,
-            errors,
-            touched,
-          }) => (
-            <Fragment>
-              <FormField
-                fieldName="firstName"
-                handleChange={() => handleChange('firstName')}
-                value={values.firstName}
-                handleBlur={() => handleBlur('firstName')}
-                placeholder="Enter First Name"
-                onFocus={() => setActiveFormId('firstName')}
-                activeFormId={activeFormId}
-              />
-
-              <FormField
-                fieldName="lastName"
-                handleChange={() => handleChange('lastName')}
-                value={values.lastName}
-                handleBlur={() => handleBlur('lastName')}
-                placeholder="Enter Last Name"
-                onFocus={() => setActiveFormId('lastName')}
-                activeFormId={activeFormId}
-              />
-
-              <FormField
-                fieldName="username"
-                handleChange={() => handleChange('username')}
-                value={values.username}
-                handleBlur={() => handleBlur('username')}
-                placeholder="Enter Unique Username"
-                onFocus={() => setActiveFormId('username')}
-                activeFormId={activeFormId}
-              />
-
-              <FormField
-                fieldName="email"
-                keyboardType="email-address"
-                placeholder="Enter Email"
-                value={values.email}
-                handleChange={() => handleChange('email')}
-                handleBlur={() => handleBlur('email')}
-                onFocus={() => setActiveFormId('email')}
-                activeFormId={activeFormId}
-              />
-
-              <FormField
-                fieldName="password"
-                handleChange={() => handleChange('password')}
-                value={values.password}
-                handleBlur={() => handleBlur('password')}
-                placeholder="Enter a strong password"
-                onFocus={() => setActiveFormId('password')}
-                activeFormId={activeFormId}
-              />
-            </Fragment>
-          )}
-        </Formik>
+        <SignUpFormik submitForm={submitForm} />
       </CommonScrollView>
     </Fragment>
   );
