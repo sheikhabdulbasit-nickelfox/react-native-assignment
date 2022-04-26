e1e054f495b1cdae66babcccb7d1896d01cc3bcc
sheikhabdulbasit-nickelfox <sheikh.abdulbasit@nickelfox.com>
2022-04-26 18:37:44 +0530

    style(AppStack): hide header

--- Source/Navigators/AppStack.js
+++ Source/Navigators/AppStack.js
@@ -1,31 +1,32 @@
 // All stack navigator of the app
-import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Routes } from "./Routes/Routes";
-import { useTheme } from "react-native-paper";
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Routes} from './Routes/Routes';
+import {useTheme} from 'react-native-paper';
 
 const Stack = createNativeStackNavigator();
 
 export const AppStacks = () => {
-  const { colors } = useTheme();
+  const {colors} = useTheme();
 
   return (
     <Stack.Navigator
       initialRouteName="Splash"
       screenOptions={{
         headerBackTitleVisible: false,
-        headerTitleAlign: "center",
+        headerTitleAlign: 'center',
         headerStyle: {
           elevation: 0,
-          shadowOpacity: 0
+          shadowOpacity: 0,
         },
         headerTitleStyle: {
-          alignSelf: "center",
+          alignSelf: 'center',
           color: colors.primary,
-          fontWeight: "400"
-        }
+          fontWeight: '400',
+        },
+        headerShown: false,
       }}>
-      {Routes.map(({ name, component, options }) => (
+      {Routes.map(({name, component, options}) => (
         <Stack.Screen
           key={name}
           name={name}
