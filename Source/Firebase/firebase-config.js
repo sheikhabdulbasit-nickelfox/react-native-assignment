8fc845eebbd401a0e8db7aad84116e877c4f569f
sheikhabdulbasit-nickelfox <sheikh.abdulbasit@nickelfox.com>
2022-04-27 14:19:31 +0530

    chore(firebase-config.js): add firebase config

---
+++ Source/Firebase/firebase-config.js
@@ -0,0 +1,12 @@
+import {initializeApp} from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAVkgsKIdA24L3wVPKhOgIgaqiXWLY5bZw',
+  authDomain: 'react-native-store-locator.firebaseapp.com',
+  projectId: 'react-native-store-locator',
+  storageBucket: 'react-native-store-locator.appspot.com',
+  messagingSenderId: '721109586385',
+  appId: '1:721109586385:web:e9dd4ff9554c4c2c5990f0',
+};
+
+const app = initializeApp(firebaseConfig);
