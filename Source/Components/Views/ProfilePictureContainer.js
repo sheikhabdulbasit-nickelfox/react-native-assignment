30ad5d2206a911024c09a2499bd37ad69afb48b9
sheikhabdulbasit-nickelfox <sheikh.abdulbasit@nickelfox.com>
2022-04-27 12:30:42 +0530

    refactor(ProfilePictureContainer): new component to display profile pic

---
+++ Source/Components/Views/ProfilePictureContainer.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {View, StyleSheet, Image, Pressable, Dimensions} from 'react-native';
+import Ionicon from 'react-native-vector-icons/Ionicons';
+import {useTheme} from 'react-native-paper';
+
+const {height} = Dimensions.get('window');
+
+const ProfilePictureContainer = ({dpImage, showImagePicker}) => {
+  const {colors} = useTheme();
+  return (
+    <View style={styles.imageContainer}>
+      <Image
+        source={
+          !dpImage.state
+            ? require('../../Resources/Images/dp.png')
+            : {uri: dpImage.uri}
+        }
+        style={[styles.profileImage, {backgroundColor: colors.primary}]}
+      />
+      <Pressable
+        onPress={showImagePicker}
+        style={[styles.cameraIcon, {backgroundColor: colors.lightAsh}]}>
+        <Ionicon name="md-camera-sharp" size={25} color={colors.surface} />
+      </Pressable>
+    </View>
+  );
+};
+
+export default ProfilePictureContainer;
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    alignSelf: 'center',
+  },
+  profileImage: {
+    margin: 30,
+    alignSelf: 'center',
+    height: height * 0.2,
+    width: height * 0.2,
+    borderRadius: 100,
+  },
+  cameraIcon: {
+    padding: 5,
+    borderRadius: 100,
+    position: 'absolute',
+    right: 35,
+    bottom: 33,
+  },
+});
