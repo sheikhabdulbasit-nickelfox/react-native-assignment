c69720d58587baf5210004ed6d3b56ff54b36753
sheikhabdulbasit-nickelfox <sheikh.abdulbasit@nickelfox.com>
2022-04-28 14:39:56 +0530

    feat(login): add form, and link to signup

--- Source/Screens/Login/Index.js
+++ Source/Screens/Login/Index.js
@@ -1,13 +1,20 @@
-import {useNavigation} from '@react-navigation/native';
 import React from 'react';
-import {Pressable, View, Text, StyleSheet} from 'react-native';
+import {View, StyleSheet, Dimensions} from 'react-native';
+import PText from '@components/Text/PText';
+import Title from '@components/Text/PTitle';
+import LoginFormik from '@components/TextInput/LoginFormik';
+import CommonScrollView from '@components/Views/CommonScrollView';
+import {useNavigation} from '@react-navigation/native';
 import {useTheme} from 'react-native-paper';
+import TextLink from '@components/Text/TextLink';
+
+const {height} = Dimensions.get('window');
 
 export default Login = props => {
   const {colors} = useTheme();
   const {reset} = useNavigation();
 
-  const navigateToLogin = () => {
+  const navigateToSignup = () => {
     reset({
       index: 0,
       routes: [{name: 'Signup'}],
@@ -15,34 +22,39 @@ export default Login = props => {
   };
 
   return (
-    <View style={styles.buttonContainer}>
-      <Pressable
-        onPress={navigateToLogin}
-        style={[styles.button, {backgroundColor: colors.primary}]}
-        android_ripple={{color: 'white', borderless: true}}>
-        <Text style={[styles.buttonLabel]}>Sign Up</Text>
-      </Pressable>
-    </View>
+    <CommonScrollView overrideStyle={styles.commonScrollView}>
+      {/* Heading */}
+      <Title
+        textAlign="center"
+        style={{marginTop: 20}}
+        bold={true}
+        variant="h1"
+        textColor="primary">
+        Sign In
+      </Title>
+
+      {/* Login Form */}
+      <View style={styles.formik}>
+        <LoginFormik />
+      </View>
+
+      {/* Direct to login */}
+      <TextLink
+        normalLabel="Not an existing user?"
+        linkLabel="Sign Up"
+        onPress={navigateToSignup}
+      />
+    </CommonScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  buttonContainer: {
-    alignSelf: 'stretch',
-    justifyContent: 'center',
-    borderRadius: 10,
-    margin: 10,
-    elevation: 25,
+  commonScrollView: {
+    marginVertical: 0,
+    marginTop: height / 15,
   },
-  button: {
-    padding: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#FFBBBB',
-    height: 40,
-  },
-  buttonLabel: {
-    fontWeight: 'bold',
-    color: 'white',
+  formik: {
+    flex: 1,
+    marginTop: height / 10,
   },
 });
