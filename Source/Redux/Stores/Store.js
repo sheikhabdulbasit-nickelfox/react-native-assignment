f97ea3a0b4b9900ab9a19181ae6bd845fb1f33df
sheikhabdulbasit-nickelfox <sheikh.abdulbasit@nickelfox.com>
2022-04-28 18:25:45 +0530

    feat(Store.js): add introscreenreducer to store

--- Source/Redux/Stores/Store.js
+++ Source/Redux/Stores/Store.js
@@ -3,6 +3,7 @@ import AppReducer from '../Reducers/AppReducer';
 import {persistStore, persistReducer} from 'redux-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import LoaderReducer from '@redux/Reducers/LoaderReducer';
+import IntroScreenReducer from '@redux/Reducers/IntroScreenReducer';
 
 const PersistConfig = {
   key: 'RNTemplate',
@@ -13,6 +14,7 @@ const PersistConfig = {
 const AllReducer = {
   app: AppReducer,
   loader: LoaderReducer,
+  intro: IntroScreenReducer,
 };
 
 const rootReducer = combineReducers(AllReducer);
