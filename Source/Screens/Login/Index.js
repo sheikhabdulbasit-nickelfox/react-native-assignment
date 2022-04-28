d8ba7521f31d893fad1dc5f9fed6a46797782f90
sheikhabdulbasit-nickelfox <sheikh.abdulbasit@nickelfox.com>
2022-04-28 15:24:13 +0530

    feat(login): add firebase sign in method

--- Source/Screens/Login/Index.js
+++ Source/Screens/Login/Index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import {View, StyleSheet, Dimensions} from 'react-native';
-import PText from '@components/Text/PText';
+import {signInWithEmailAndPassword} from 'firebase/auth';
+import {auth} from '../../../Firebase/firebase-config';
 import Title from '@components/Text/PTitle';
 import LoginFormik from '@components/TextInput/LoginFormik';
 import CommonScrollView from '@components/Views/CommonScrollView';
 import {useNavigation} from '@react-navigation/native';
-import {useTheme} from 'react-native-paper';
 import TextLink from '@components/Text/TextLink';
+import AppDispatcher from '@redux/Dispatchers/AppDispatcher';
 
 const {height} = Dimensions.get('window');
 
 export default Login = props => {
-  const {colors} = useTheme();
   const {reset} = useNavigation();
 
   const navigateToSignup = () => {
@@ -21,12 +21,33 @@ export default Login = props => {
     });
   };
 
+  const userLogin = values => {
+    signInWithEmailAndPassword(auth, values.email, values.password)
+      .then(res => {
+        if (res.user.accessToken) {
+          const user = auth.currentUser;
+          const data = {
+            tokens: user.accessToken,
+            user: {
+              name: user.displayName,
+              email: user.email,
+              uid: user.uid,
+              photoURL: user.photoURL,
+            },
+          };
+          console.log(res);
+          AppDispatcher.setUserLoggedIn(data);
+        }
+      })
+      .then(() => {});
+  };
+
   return (
     <CommonScrollView overrideStyle={styles.commonScrollView}>
       {/* Heading */}
       <Title
         textAlign="center"
-        style={{marginTop: 20}}
+        style={styles.textHeading}
         bold={true}
         variant="h1"
         textColor="primary">
@@ -35,7 +56,7 @@ export default Login = props => {
 
       {/* Login Form */}
       <View style={styles.formik}>
-        <LoginFormik />
+        <LoginFormik submitForm={userLogin} />
       </View>
 
       {/* Direct to login */}
@@ -51,10 +72,12 @@ export default Login = props => {
 const styles = StyleSheet.create({
   commonScrollView: {
     marginVertical: 0,
-    marginTop: height / 15,
   },
   formik: {
     flex: 1,
+    marginTop: height / 10,
+  },
+  textHeading: {
     marginTop: height / 10,
   },
 });
