3a7f90cb9cc57bf92d44d919724fabc0aafbc07e
sheikhabdulbasit-nickelfox <sheikh.abdulbasit@nickelfox.com>
2022-05-05 18:08:49 +0530

    fix(AppDispatcher): change import statement

--- Source/Redux/Dispatchers/AppDispatcher.js
+++ Source/Redux/Dispatchers/AppDispatcher.js
@@ -1,18 +1,18 @@
 // App wide dispatcher
 
-import Stores from "../Stores/Store";
-import Actions from "../Actions/AppActions";
+import {Stores} from '../Stores/Store';
+import Actions from '../Actions/AppActions';
 
 export default AppDispatcher = {
   setUserLoggedIn: data => {
-    Stores.dispatch({ type: Actions.SET_TOKEN, data: data.tokens });
-    Stores.dispatch({ type: Actions.LOGIN, data: data.user });
+    Stores.dispatch({type: Actions.SET_TOKEN, data: data.tokens});
+    Stores.dispatch({type: Actions.LOGIN, data: data.user});
   },
   setUserLoggedOut: () => {
-    Stores.dispatch({ type: Actions.SET_USER_FUE, data: false });
-    Stores.dispatch({ type: Actions.LOGOUT });
+    Stores.dispatch({type: Actions.SET_USER_FUE, data: false});
+    Stores.dispatch({type: Actions.LOGOUT});
   },
   updateUserTokens: data => {
-    Stores.dispatch({ type: Actions.SET_TOKEN, data });
-  }
+    Stores.dispatch({type: Actions.SET_TOKEN, data});
+  },
 };
