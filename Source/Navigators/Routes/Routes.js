33de04b8f875ce22ea4af0836ad20fd8d174b33f
sheikhabdulbasit-nickelfox <sheikh.abdulbasit@nickelfox.com>
2022-05-05 18:07:49 +0530

    fix(Routes): change dsahboard import

--- Source/Navigators/Routes/Routes.js
+++ Source/Navigators/Routes/Routes.js
@@ -1,14 +1,14 @@
-import { RouteModel } from "./Models/RouteModel";
-import Login from "../../Screens/Login/Index";
-import Intro from "../../Screens/Onboarding/Introduction/Intro";
-import Splash from "../../Screens/Onboarding/Splash/Splash";
-import Signup from "../../Screens/Signup/Signup";
-import Dashboard from "@screens/UserDashboard";
+import {RouteModel} from './Models/RouteModel';
+import Login from '../../Screens/Login/Index';
+import Intro from '../../Screens/Onboarding/Introduction/Intro';
+import Splash from '../../Screens/Onboarding/Splash/Splash';
+import Signup from '../../Screens/Signup/Signup';
+import Dashboard from '../../Screens/UserDashboard/index';
 
 export const Routes = [
-  new RouteModel("Splash", Splash, { headerShown: false }),
-  new RouteModel("Intro", Intro, { headerShown: false }),
-  new RouteModel("Signup", Signup),
-  new RouteModel("Login", Login, { headerTitle: "Login" }),
-  new RouteModel("Dashboard", Dashboard, { headerTitle: "Home" })
+  new RouteModel('Splash', Splash, {headerShown: false}),
+  new RouteModel('Intro', Intro, {headerShown: false}),
+  new RouteModel('Signup', Signup),
+  new RouteModel('Login', Login, {headerTitle: 'Login'}),
+  new RouteModel('Dashboard', Dashboard, {headerTitle: 'Home'}),
 ];
