43aba11b4460ee845906de32eb3c729738e52034
sheikhabdulbasit-nickelfox <sheikh.abdulbasit@nickelfox.com>
2022-05-05 18:11:57 +0530

    fix(Store: change variable names, use blacklist instead of whitelist)

--- Source/Redux/Stores/Store.js
+++ Source/Redux/Stores/Store.js
@@ -1,25 +1,25 @@
 import {createStore, combineReducers} from 'redux';
-import AppReducer from '../Reducers/AppReducer';
-import {persistStore, persistReducer} from 'redux-persist';
+import persistReducer from 'redux-persist/es/persistReducer';
+import persistStore from 'redux-persist/es/persistStore';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import AppReducer from '../Reducers/AppReducer';
 import LoaderReducer from '@redux/Reducers/LoaderReducer';
-import IntroScreenReducer from '@redux/Reducers/IntroScreenReducer';
+import IntroScreenReducer from '../Reducers/IntroScreenReducer';
 
-const PersistConfig = {
-  key: 'RNTemplate',
-  storage: AsyncStorage,
-  whitelist: ['app'],
-};
-
-const AllReducer = {
+const AllReducers = {
   app: AppReducer,
   loader: LoaderReducer,
   intro: IntroScreenReducer,
 };
 
-const rootReducer = combineReducers(AllReducer);
-const persistedReducer = persistReducer(PersistConfig, rootReducer);
+const PersistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  blacklist: ['loader'],
+};
 
-const Stores = createStore(persistedReducer);
+const mergedReducer = combineReducers(AllReducers);
+const persistedReducer = persistReducer(PersistConfig, mergedReducer);
+
+export const Stores = createStore(persistedReducer);
 export const Persistor = persistStore(Stores);
-export default Stores;
